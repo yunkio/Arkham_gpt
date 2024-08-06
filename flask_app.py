7d3a46866cef8d5b08dd49ac4c09b34f65fe585e
yun <yko2222@naver.com>
2024-08-06 17:24:34 +0900

    dd

--- flask_app.py
+++ flask_app.py
@@ -61,4 +61,5 @@ def index():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    socketio.run(app, host='0.0.0.0', port=port)
+    socketio.run(app, host='0.0.0.0', port=port, allow_unsafe_werkzeug=True)
+    
